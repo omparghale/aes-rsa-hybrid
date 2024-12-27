403a139496ebc31268605a5d3ad6641515a24d97
Om Parghale <omparghale75@gmail.com>
2024-12-28 04:34:23 +0530

    added headers and functions

--- include/encoding_utils.h
+++ include/encoding_utils.h
@@ -1,15 +1,17 @@
 #ifndef ENCODING_UTILS_H
 #define ENCODING_UTILS_H
 
-#include <vector>
+#include <iostream>
 #include <string>
-#include<bitset>
+#include <bitset>
+#include<cstdint>
 
 
 std::string ascii2text_str(uint64_t num);
 std::string ascii2text_str_file_read(const std::string &path, char delimeter);
 std::string text2ascii_str(std::string s);
 uint64_t text2ascii_int(std::string s);
+std::string int2base64(uint64_t num);
 
 #endif
 
