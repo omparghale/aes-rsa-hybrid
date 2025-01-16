ca9d643902b307185212399ac9df6209f3d2389d
Om Parghale <130880556+omparghale@users.noreply.github.com>
2025-01-16 20:35:46 +0530

    removed unused function declarations

--- include/encoding_utils.h
+++ include/encoding_utils.h
@@ -16,17 +16,4 @@ void create_digest(const unsigned char *message, size_t message_len,
 
 // Computes a SHA256 hash of a string and returns it in hexadecimal format
 std::string sha256str(const std::string &msg);
-
-// Converts a 64 bit ASCII value to its string representation
-std::string ascii2text_str(uint64_t num);
-
-// Reads ASCII values from a file and converts them into a string
-std::string ascii2text_str_file_read(const std::string &path, char delimeter);
-
-// Converts a string to its ASCII representation as a concatenated string
-std::string text2ascii_str(std::string s);
-
-// Converts a string to its ASCII representation as a 64-bit integer
-uint64_t text2ascii_int(std::string s);
-
 #endif
