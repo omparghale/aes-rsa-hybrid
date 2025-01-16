64f55fab5d61209509b5f259383adb58d7b6d42f
Om Parghale <130880556+omparghale@users.noreply.github.com>
2025-01-16 20:34:48 +0530

    removed padding function declarations

--- include/aes_util.h
+++ include/aes_util.h
@@ -6,8 +6,6 @@
 typedef unsigned char byte;
 
 std::vector<byte> generate16bytes();
-void pkcs7_pad(std::vector<byte> &input);
-void pkcs7_unpad(std::vector<byte> &input, bool test_mode);
 int enc_aes_128_cbc(const std::string &og_str,
                     const std::vector<byte> &key,
                     const std::vector<byte> &iv,
